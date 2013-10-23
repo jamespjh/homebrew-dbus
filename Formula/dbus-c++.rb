ad70267d9f4ea90b4ebc7dee3ca099957a04c44c
jamespjh <jamespjh@googlemail.com>
2013-10-23 17:57:52 +0100

    Discovery of various problems

--- Formula/dbus-c++.rb
+++ Formula/dbus-c++.rb
@@ -19,8 +19,11 @@ class DbusCxx <Formula
     # Fix the TMPDIR to one D-Bus doesn't reject due to odd symbols
     ENV["TMPDIR"] = "/tmp"
     system "sed -i -e 's/libtool/glibtool/' bootstrap"
-    system "./bootstrap"
+    system "sed -i -e s/PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP/PTHREAD_RECURSIVE_MUTEX_INITIALIZER/ src/eventloop.cpp"
+    system "autoreconf -sif"
     system "./configure", "--disable-ecore", "--prefix=#{prefix}", "--disable-doxygen-docs"
+    # Need to add <unistd.h> to get unix headers
+    # and need to apply the patch from https://code.google.com/p/chromium/issues/detail?id=217426
     system "make install"
 
     # Generate D-Bus's UUID for this machine
